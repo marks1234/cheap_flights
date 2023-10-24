f27028f4f27d0c3b39aedfd0ec1d3fc190ded47c
hmahar <harrimahar@gmail.com>
2023-10-24 04:19:26 +0300

    Added collt library

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gocolly/colly"
+)
 
 func main() {
-	fmt.Println("Hello Cheap Flights!")
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit("http://go-colly.org/")
 }
