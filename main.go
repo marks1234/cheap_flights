a9b23b9781831c8f2f4e35fbeebf6e9e1cf09718
hmahar <harrimahar@gmail.com>
2023-10-24 08:37:00 +0300

    testing api

--- main.go
+++ main.go
@@ -2,21 +2,42 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/gocolly/colly"
+	"io"
+	"net/http"
 )
 
 func main() {
-	c := colly.NewCollector()
+	// Replace with your Amadeus API key
+	apiKey := "NhOQ7AAh3Rqhr3BbDLVYtUP4ZHIV"
 
-	// Find and visit all links
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		e.Request.Visit(e.Attr("href"))
-	})
+	// Define the API endpoint URL
+	endpoint := "https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=MAD"
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+	// Construct the request URL with query parameters
+	req, err := http.NewRequest("GET", endpoint, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
 
-	c.Visit("http://go-colly.org/")
+	// Set the API key in the request headers
+	req.Header.Add("Authorization", "Bearer "+apiKey)
+
+	// Create an HTTP client and send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// Read and print the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
